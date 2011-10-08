97dbf92bfdf6a2454538683980a1cb8c65a94b2e
Christian Peters <christian.peters@zweitag.de>
2011-10-08 19:59:41 +0200

    Add templates including manifest and grid configuration template

---
+++ templates/project/manifest.rb
@@ -0,0 +1,25 @@
+stylesheet '_grid.sass', :media => 'screen, projection'
+
+description "A Compass-based Grid that strives for lean & sane CSS and supports complex layouts"
+
+help %Q{
+
+  --- Crispy Grid ---
+
+  A Compass-based Grid that strives for lean & sane CSS and supports complex layouts
+
+  For documentation, see crispy/_grid.sass
+
+}
+
+welcome_message %Q{
+
+
+  --- Crispy Grid ---
+
+  A Compass-based Grid that strives for lean & sane CSS and supports complex layouts
+
+  For documentation, see crispy/_grid.sass
+
+}
+
